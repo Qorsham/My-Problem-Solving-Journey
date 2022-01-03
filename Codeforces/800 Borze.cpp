3c4ab4028f81321187dc52287726337fc54fc22a
Qorsham <HatemQorsham@gmail.com>
2022-01-03 22:59:51 +0200

    Create 800 Borze.cpp

---
+++ Codeforces/800 Borze.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+int main() {
+  std::string str,out;std::cin>>str;
+  for(int i=0;i<str.size();i++)
+  {
+    if(str[i]=='.')
+    {
+      out.push_back('0');
+    }
+    else if(str[i]=='-'&&str[i+1]=='.'){
+    	out.push_back('1');
+    	i++;
+	}
+	else if(str[i]=='-'&&str[i+1]=='-'){
+    	out.push_back('2');
+    	i++;
+  	}
+  }
+  std::cout<<out;
+}
