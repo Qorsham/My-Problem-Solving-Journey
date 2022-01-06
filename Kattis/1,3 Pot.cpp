9ab29a1cec60ebd601a46df5d3f0982320e7e2ad
Qorsham <HatemQorsham@gmail.com>
2022-01-06 06:05:29 +0200

    Create 1,3 Pot.cpp

---
+++ Kattis/1,3 Pot.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include <cmath>
+#include <string>
+#include <sstream>
+int main() 
+{
+	int n,out=0;std::cin>>n;std::string str;
+	for(int i=0;i<n;i++)
+	{
+		int x;std::cin>>str;
+		std::string str1 = str.substr(0,str.size()-1);
+		std::stringstream ss(str1);
+	    ss >> x;
+		char str2 = str[str.size()-1];
+		out+=pow(x,(int)str2-48);
+	}
+	std::cout<<out;
+}
