89309f86de1677466790667b3134db252e18ba8a
Qorsham <HatemQorsham@gmail.com>
2022-01-08 09:27:15 +0200

    Create 1.3 Bijele.cpp

---
+++ Kattis/1.3 Bijele.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+int main() 
+{
+	int ki=1,qu=1,ro=2,bi=2,kn=2,ei=8;
+	int iki,iqu,iro,ibi,ikn,iei;
+	std::cin>>iki>>iqu>>iro>>ibi>>ikn>>iei;
+	std::cout<<ki-iki<<" "<<qu-iqu<<" "<<ro-iro<<" "<<
+	bi-ibi<<" "<<kn-ikn<<" "<<ei-iei;
+}
